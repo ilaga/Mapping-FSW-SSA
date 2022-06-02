7ce0b46db2b84f803857d899461912b16142c0b4
Ian Laga <ilaga25@gmail.com>
2022-06-01 22:41:33 -0400

    Update figures

--- R/Plotting 04/Result_Plotting_10.R
+++ R/Plotting 04/Result_Plotting_10.R
@@ -9,6 +9,7 @@
 
 
 library(tmap) # https://cran.r-project.org/web/packages/tmap/vignettes/tmap-getstarted.html
+tmap_options(check.and.fix = TRUE)
 ## tmaptools::palette_explorer()
 library(spData)
 library(raster)
@@ -16,9 +17,10 @@ library(spdep)
 library(cartography)
 library(ggplot2)
 
+
 # Load Results and shapes -------------------------------------------------
 
-pse.dat = read.csv("../Data/PSE Spreadsheets/SSA_FSW_PSE_03.csv")
+pse.dat = read.csv("../Data/FSW Spreadsheets/SSA_FSW_PSE_03.csv")
 pse.country = subset(pse.dat, Validated_area == "National")
 
 africa.iso = readxl::read_excel("../Data/Africa_ISO.xlsx")
@@ -43,10 +45,14 @@ country.shape.patterns <- st_sf(geometry = country.shape.sf.2)
 
 
 gadm.raw = readRDS("../Data/Shapefile_Prep.rds")
-gadm.district = readRDS("./Data/Results/Final_district_est.rds")
-gadm.country = readRDS("./Data/Results/Final_country_est.rds")
-pse.dat.fit = readRDS("../Data/Final_pse_dat_fit_3.rds")
+gadm.district = readRDS("../Data/Results/Final_district_est.rds")
+gadm.country = readRDS("../Data/Results/Final_country_est.rds")
+pse.dat.fit = readRDS("../Data/Final_pse_dat_fit.rds")
 pse.dat.sub = subset(pse.dat.fit, !is.na(pse))
+
+gadm.district$Percent = round(gadm.district$Prev * 100, digits = 2)
+gadm.country$Percent = round(gadm.country$Percent, digits = 2)
+
 
 pse.prev.iso.dat = pse.dat.sub
 pse.prev.iso.dat$ISO = factor(pse.prev.iso.dat$ISO, levels = levels(pse.dat.fit$ISO))
@@ -61,11 +67,11 @@ gg.prev.iso = ggplot(pse.dat.sub) +
   scale_color_manual(values = cbPalette,
                      name = "\n", labels = c("Sub-national", "City")) +
   theme(legend.title=element_blank(),
-        axis.text = element_text(size = 18),
+        axis.text = element_text(size = 22, face = "bold"),
         axis.title = element_text(size = 22, face = "bold"),
         axis.text.x = element_text(angle = 90, vjust = 0.5),
-        legend.text = element_text(size = 18))
-ggsave("./Figures/Prev_ISO.jpg", gg.prev.iso, width = 20, height = 10)
+        legend.text = element_text(size = 22, face = "bold"))
+ggsave("../Figures/Prev_ISO.jpg", gg.prev.iso, width = 20, height = 10)
 
 
 
@@ -73,26 +79,35 @@ pse.dat.sub$City = factor(pse.dat.sub$City, levels = c(0,1), labels = c("Sub-nat
 gg.prev.refpop = ggplot(pse.dat.sub) + geom_point(aes(x = ref_pop_log, y = logit_prev)) +
   xlab("Log-transformed reference population") + ylab("Logit-transformed FSW proportion") +
   theme_grey(base_size = 15) + facet_wrap(~City) +
-  theme(axis.text = element_text(size = 18),
+  theme(axis.text = element_text(size = 22, face = "bold"),
         axis.title = element_text(size = 22, face = "bold"),
-        strip.text.x = element_text(size = 18))
+        strip.text.x = element_text(size = 22, face = "bold"),
+        aspect.ratio = 1)
 gg.prev.refpop
-ggsave("./Figures/FSW_prop_ref_pop.jpg", gg.prev.refpop, width = 20, height = 10)
+ggsave("../Figures/FSW_prop_ref_pop.jpg", gg.prev.refpop, width = 20, height = 10)
 
 
 
 
 gg.prev.pse = ggplot(pse.dat.sub) + geom_point(aes(y = logit_prev, x = log(pse))) +
   ylab("Logit-transformed FSW proportion") + xlab("Log-transformed FSW PSE") +
-  theme_grey(base_size = 15)
+  theme_grey(base_size = 15) +
+  theme(axis.text = element_text(size = 22, face = "bold"),
+        axis.title = element_text(size = 22, face = "bold"),
+        strip.text.x = element_text(size = 22, face = "bold"),
+        aspect.ratio = 1)
 gg.prev.pse
 
 gg.refpop.pse = ggplot(pse.dat.sub) + geom_point(aes(y = ref_pop_log, x = log(pse))) +
   ylab("Log-transformed reference population") + xlab("Log-transformed FSW PSE") +
-  theme_grey(base_size = 15)
+  theme_grey(base_size = 15) +
+  theme(axis.text = element_text(size = 22, face = "bold"),
+        axis.title = element_text(size = 22, face = "bold"),
+        strip.text.x = element_text(size = 22, face = "bold"),
+        aspect.ratio = 1)
 gg.refpop.pse
 
-gridExtra::grid.arrange(gg.prev.pse, gg.refpop.pse, gg.prev.refpop, nrow = 1)
+gridExtra::grid.arrange(gg.prev.pse, gg.refpop.pse, gg.prev.refpop, nrow = 2)
 
 
 
@@ -111,33 +126,6 @@ gg.prev.walking = ggplot(pse.dat.sub) + geom_point(aes(x = walking_healthcare_po
   xlab("Walking healthcare") + ylab("Logit-transformed FSW proportion") +
   theme_grey(base_size = 15) + facet_wrap(~City)
 gg.prev.walking
-
-
-
-# Plot empirical prevalence -----------------------------------------------
-gadm.district$Prev_emp = NA
-for(i in 1:nrow(gadm.district)){
-  match.ind = which(pse.dat.sub$ID == gadm.district$ID[i])
-  if(length(match.ind) > 0){
-    gadm.district$Prev_emp[i] = mean(pse.dat.sub$prev[match.ind], na.rm = T)
-  }
-}
-
-
-empirical.prev = tm_shape(country.shape.africa) +
-  tm_polygons("na", colorNA = "grey40", legend.show = FALSE) +
-  tm_shape(country.shape.patterns) + 
-  tm_lines(col = "grey20") +
-  tm_shape(gadm.district) +
-  tm_polygons("Prev_emp", n = 10, style = "quantile",
-              colorNA = "grey70",
-              title = "Observed Proportion",
-              border.col = "transparent",
-              legend.show = TRUE) +
-  tm_layout(frame = FALSE) +
-  tm_shape(gadm.country) +
-  tm_borders()
-
 
 
 
@@ -169,6 +157,7 @@ all.avail.map = tm_shape(country.shape.africa) +
               palette = "Reds",
               contrast = c(0.3,1),
               colorNA = "grey70",
+              textNA = "0",
               title = "Number of\ntotal PSE",
               border.col = "transparent",
               legend.show = TRUE) +
@@ -190,6 +179,7 @@ city.avail.map = tm_shape(country.shape.africa) +
               palette = "Reds",
               contrast = c(0.3,1),
               colorNA = "grey70",
+              textNA = "0",
               title = "Number of\ncity PSE",
               border.col = "transparent",
               legend.show = TRUE) +
@@ -212,6 +202,7 @@ district.avail.map = tm_shape(country.shape.africa) +
               palette = "Reds",
               contrast = c(0.3, 1),
               colorNA = "grey70",
+              textNA = "0",
               title = "Number of\nsub-national PSE",
               border.col = "transparent",
               legend.show = TRUE) +
@@ -225,12 +216,14 @@ district.avail.map = tm_shape(country.shape.africa) +
 
 
 
+
+
 country.prev.map = tm_shape(country.shape.africa) +
   tm_polygons("na", colorNA = "grey40", legend.show = FALSE) +
   tm_shape(country.shape.patterns) + 
   tm_lines(col = "grey20") +
   tm_shape(gadm.country) +
-  tm_polygons("Prev", title = "FSW Proportion",
+  tm_polygons("Percent", title = "FSW Percent",
               n = 4, style = "quantile",
               textNA = NA) +
   tm_layout(frame = FALSE,
@@ -257,7 +250,7 @@ district.prev.map = tm_shape(country.shape.africa) +
   tm_shape(country.shape.patterns) + 
   tm_lines(col = "grey20") +
   tm_shape(gadm.district) +
-  tm_polygons("Prev", title = "FSW Proportion", n = 10, style = "quantile",
+  tm_polygons("Percent", title = "FSW Percent", n = 10, style = "quantile",
               border.col = "transparent",
               showNA = FALSE) +
   tm_layout(frame = FALSE,
@@ -272,7 +265,7 @@ district.prev.even.map = tm_shape(country.shape.africa) +
   tm_shape(country.shape.patterns) + 
   tm_lines(col = "grey20") +
   tm_shape(gadm.district) +
-  tm_polygons("Prev", title = "FSW Proportion", breaks = seq(0, 0.11, by = 0.01),
+  tm_polygons("Percent", title = "FSW Percent", breaks = seq(0, 0.11, by = 0.01),
               border.col = "transparent",
               showNA = FALSE) +
   tm_layout(frame = FALSE,
@@ -339,68 +332,71 @@ district.uncertainty.even.map = tm_shape(country.shape.africa) +
 # Save maps ---------------------------------------------------------------
 
 tmap_save(all.avail.map,
-          filename = "./Figures/Total_Availability.jpg",
+          filename = "../Figures/Total_Availability.jpg",
           dpi = 1000, asp = NA)
 tmap_save(city.avail.map,
-          filename = "./Figures/City_Availability.jpg",
+          filename = "../Figures/City_Availability.jpg",
           dpi = 1000, asp = NA)
 tmap_save(district.avail.map,
-          filename = "./Figures/District_Availability.jpg",
+          filename = "../Figures/District_Availability.jpg",
           dpi = 1000, asp = NA)
 tmap_save(country.prev.map,
-          filename = "./Figures/Country_Prevalence.jpg",
+          filename = "../Figures/Country_Prevalence.jpg",
           dpi = 1000, asp = NA)
 tmap_save(country.uncertainty.map,
-          filename = "./Figures/Country_Uncertainty.jpg",
+          filename = "../Figures/Country_Uncertainty.jpg",
           dpi = 1000, asp = NA)
 tmap_save(district.prev.map,
-          filename = "./Figures/District_Prevalence.jpg",
+          filename = "../Figures/District_Prevalence.jpg",
           dpi = 1000, asp = NA)
 tmap_save(district.uncertainty.map,
-          filename = "./Figures/District_Uncertainty.jpg",
+          filename = "../Figures/District_Uncertainty.jpg",
           dpi = 1000, asp = NA)
 
 
 ## Interactive plots: RUN WITH CARE
-gadm.country$Lowerp = gadm.country$Lower * 100
-gadm.country$Upperp = gadm.country$Upper * 100
-
-gadm.district$Percent = gadm.district$Prev * 100
-gadm.district$Upperp = gadm.district$Upper * 100
-gadm.district$Lowerp = gadm.district$Lower * 100
-
-tmap_mode("view")
-both.map = tm_shape(country.shape.africa) +
-  tm_borders(group = NULL) +
-  tm_shape(gadm.country, name = "Country Estimates") +
-  tm_polygons("Percent", title = "Country FSW Proportion (%)",
-              n = 4, style = "quantile",
-              popup.vars = c("ISO-3" = "ISO",
-                             "Estimated FSW Proportion (%)" = "Percent",
-                             "Lower Bound (%)" = "Lowerp",
-                             "Upper Bound (%)" = "Upperp"),
-              popup.format=list(Percent = list(digits = 2),
-                                Lowerp = list(digits = 2),
-                                Upperp = list(digits = 2)),
-              id = "NAME_0") +
-  tm_shape(gadm.district, name = "Sub-national Estimates") +
-  tm_polygons("Percent", title = "Sub-national FSW Proportion (%)", n = 10, style = "quantile",
-              border.col = "transparent",
-              popup.vars = c("Country" = "NAME_0",
-                            "ISO-3" = "ISO",
-                             "Estimated FSW Proportion (%)" = "Percent",
-                             "Lower Bound (%)" = "Lowerp",
-                             "Upper Bound (%)" = "Upperp"),
-              popup.format=list(Percent = list(digits = 2),
-                                Lowerp = list(digits = 2),
-                                Upperp = list(digits = 2)),
-              id = "NAME_1") + 
-  tm_layout(frame = FALSE,
-            legend.title.size = 1.5,
-            legend.text.size = 1.5,
-            legend.title.fontface = 2, asp = NA) +
-  tm_shape(gadm.country, name = "Country Borders") +
-  tm_borders(group = NULL) +
-  tm_facets(as.layers = TRUE)
-tmap_save(both.map,
-          filename = "./Figures/FSW_Map_Interactive.html")
+if(0){
+  gadm.country$Lowerp = gadm.country$Lower * 100
+  gadm.country$Upperp = gadm.country$Upper * 100
+  
+  gadm.district$Percent = gadm.district$Prev * 100
+  gadm.district$Upperp = gadm.district$Upper * 100
+  gadm.district$Lowerp = gadm.district$Lower * 100
+  
+  tmap_mode("view")
+  both.map = tm_shape(country.shape.africa) +
+    tm_borders(group = NULL) +
+    tm_shape(gadm.country, name = "Country Estimates") +
+    tm_polygons("Percent", title = "Country FSW Percent (%)",
+                n = 4, style = "quantile",
+                popup.vars = c("ISO-3" = "ISO",
+                               "Estimated FSW Percent (%)" = "Percent",
+                               "Lower Bound (%)" = "Lowerp",
+                               "Upper Bound (%)" = "Upperp"),
+                popup.format=list(Percent = list(digits = 2),
+                                  Lowerp = list(digits = 2),
+                                  Upperp = list(digits = 2)),
+                id = "NAME_0") +
+    tm_shape(gadm.district, name = "Sub-national Estimates") +
+    tm_polygons("Percent", title = "Sub-national FSW Percent (%)", n = 10, style = "quantile",
+                border.col = "transparent",
+                popup.vars = c("Country" = "NAME_0",
+                               "ISO-3" = "ISO",
+                               "Estimated FSW Percent (%)" = "Percent",
+                               "Lower Bound (%)" = "Lowerp",
+                               "Upper Bound (%)" = "Upperp"),
+                popup.format=list(Percent = list(digits = 2),
+                                  Lowerp = list(digits = 2),
+                                  Upperp = list(digits = 2)),
+                id = "NAME_1") + 
+    tm_layout(frame = FALSE,
+              legend.title.size = 1.5,
+              legend.text.size = 1.5,
+              legend.title.fontface = 2, asp = NA) +
+    tm_shape(gadm.country, name = "Country Borders") +
+    tm_borders(group = NULL) +
+    tm_facets(as.layers = TRUE)
+  tmap_save(both.map,
+            filename = "./Figures/FSW_Map_Interactive.html")
+  
+}
